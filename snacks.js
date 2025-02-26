24b3f0359e05432294ccfe1e83c79f45d3f612a2
Gianluca-Milesi <gianluca.milesi.99@gmail.com>
2025-02-26 15:35:13 +0100

    ADD: Snack 1 (Bonus)

--- snacks.js
+++ snacks.js
@@ -4,11 +4,33 @@ function getPostTitle(id) {
     return new Promise((resolve, reject) => {
         fetch(`https://dummyjson.com/posts/${id}`)
             .then(response => response.json())
-            .then(oggetto => resolve(oggetto))
+            .then(oggetto => resolve(oggetto.title))
             .catch(reject)
     })
 }
 
 getPostTitle(2)
-    .then(oggetto => console.log(oggetto.title))
+    .then(oggetto => console.log(oggetto))
+    .catch(error => console.error(error))
+
+//Bonus: Ottieni l'intero post con l'autore
+function getPost(id) {
+    return new Promise((resolve, reject) => {
+        fetch(`https://dummyjson.com/posts/${id}`)
+            .then(response => response.json())
+            .then(post => {
+                fetch(`https://dummyjson.com/users/${post.userId}`)
+                    .then(response => response.json())
+                    .then(user => {
+                        post.user = user
+                        resolve(post)
+                    })
+                    .catch(reject)
+            })
+            .catch(reject)
+    })
+}
+
+getPost(1)
+    .then(post => console.log(post))
     .catch(error => console.error(error))
